78c9c42dd189b8c3b576ec46a401556e58f8cc77
Jenny <jenny@youwza.com>
2021-10-19 10:49:41 +0200

    Skills content, js and css

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -4,45 +4,45 @@ window.onscroll = () => {
   stickIt();
 };
 
-const navbar = document.querySelector("nav");
-const nav = document.querySelector(".nav__wrap");
-const menuLinks = document.querySelectorAll(".nav__link");
-const sections = document.querySelectorAll("section");
+const navbar = document.querySelector('.nav');
+const nav = document.querySelector('.nav__wrap');
+const menuLinks = document.querySelectorAll('.nav__link');
+const sections = document.querySelectorAll('section');
 
 const sticky =
-  navbar.offsetTop > sections[1].offsetTop - 100 ?
-  navbar.offsetTop :
-  sections[1].offsetTop - 53;
+  navbar.offsetTop > sections[1].offsetTop - 100
+    ? navbar.offsetTop
+    : sections[1].offsetTop - 53;
 
 const stickIt = () => {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
+    navbar.classList.add('sticky');
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove('sticky');
   }
 };
 
 /* Handle scrolls */
 
 let scrollpos = window.scrollY;
-let activePage = "home";
+let activePage = 'home';
 
-const add_nav_class = () => navbar.classList.add("fade-in");
-const remove_nav_class = () => navbar.classList.remove("fade-in");
+const add_nav_class = () => navbar.classList.add('fade-in');
+const remove_nav_class = () => navbar.classList.remove('fade-in');
 
 const setActiveLink = (activePage) => {
   let target;
   menuLinks.forEach((link) => {
-    target = link.querySelector("a").getAttribute("href").replace(/[^\w]/, "");
+    target = link.querySelector('a').getAttribute('href').replace(/[^\w]/, '');
     if (target === activePage) {
-      link.classList.add("active");
+      link.classList.add('active');
     } else {
-      link.classList.remove("active");
+      link.classList.remove('active');
     }
   });
 };
 
-window.addEventListener("scroll", function() {
+window.addEventListener('scroll', function () {
   scrollpos = window.scrollY;
 
   if (scrollpos >= sticky) {
@@ -53,7 +53,7 @@ window.addEventListener("scroll", function() {
 
   sections.forEach((section) => {
     if (scrollpos >= section.offsetTop - 100) {
-      activePage = section.getAttribute("id");
+      activePage = section.getAttribute('id');
     }
   });
   if (history.pushState) {
@@ -71,18 +71,18 @@ const toggleMenu = () => {
     window.scrollTo(0, sections[1].offsetTop);
   }
 
-  if (nav.classList.contains("visible")) {
-    nav.classList.remove("visible");
+  if (nav.classList.contains('visible')) {
+    nav.classList.remove('visible');
   } else {
-    nav.classList.add("visible");
+    nav.classList.add('visible');
   }
 };
 
-const menuIcon = document.querySelector(".menu-icon");
+const menuIcon = document.querySelector('.menu-icon');
 
-menuIcon.addEventListener("click", toggleMenu);
+menuIcon.addEventListener('click', toggleMenu);
 
 menuLinks.forEach((link) => {
-  link.addEventListener("click", toggleMenu);
-  link.addEventListener("click", remove_nav_class);
+  link.addEventListener('click', toggleMenu);
+  link.addEventListener('click', remove_nav_class);
 });
--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,9 +1,41 @@
-const parent = document.getElementById("skills");
-const tags = parent.getElementsByTagName("skills__item");
+const parent = document.getElementById("skills__items");
+const tags = parent.getElementsByTagName("li");
+const buttons = document.getElementsByClassName("skills__button");
+const buttonReLoad = document.querySelector(".skills__button--reload");
+const buttonSort = document.querySelector(".skills__button--sort");
 const fragment = document.createDocumentFragment();
 
-// Randomize order of skills
-while (tags.length) {
-  fragment.appendChild(tags[Math.floor(Math.random() * tags.length)]);
+const renderTags = () => {
+  while (tags.length) {
+    const idx = Math.floor(Math.random() * tags.length);
+    fragment.appendChild(tags[idx]);
+  }
+  parent.appendChild(fragment);
+};
+
+const filterTags = (category) => {
+  for (let idx = 0; idx < tags.length; idx++) {
+    if (!category || tags[idx].dataset.skillsCategory === category) {
+      tags[idx].classList.remove("skills__item--faded");
+    } else {
+      tags[idx].classList.add("skills__item--faded");
+    }
+  }
+};
+
+const sortTags = () => {
+  Array.from(tags)
+    .sort((a, b) => a.textContent.localeCompare(b.textContent))
+    .forEach((li) => parent.appendChild(li));
+};
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("mouseover", () => {
+    filterTags(buttons[i].dataset.skillsCategory);
+  });
+  buttons[i].addEventListener("mouseout", () => filterTags());
 }
-parent.appendChild(fragment);
+buttonReLoad.addEventListener("click", renderTags);
+buttonSort.addEventListener("click", sortTags);
+
+renderTags();
