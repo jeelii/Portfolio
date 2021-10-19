3ce41d52e7d0898c29972e3f1316045a40d394ce
Jenny <jenny@youwza.com>
2021-10-19 10:44:37 +0200

    Add favicon to dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,28 +1,31 @@
-const path = require("path");
-const common = require("./webpack.common");
-const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: "development",
+  mode: 'development',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
-    hotUpdateChunkFilename: "hot/hot-update.js",
-    hotUpdateMainFilename: "hot/hot-update.json",
+    hotUpdateChunkFilename: 'hot/hot-update.js',
+    hotUpdateMainFilename: 'hot/hot-update.json',
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.hbs",
+      template: './src/index.hbs',
+      favicon: './src/assets/favicon.ico',
     }),
   ],
   module: {
-    rules: [{
-      test: /\.(scss|css)$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-    }, ],
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
-  devtool: "source-map",
+  devtool: 'source-map',
 });
