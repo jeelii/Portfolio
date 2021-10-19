53b5cce255242c138c7c562bf86d8d43a8c43c4c
Jenny <jenny@youwza.com>
2021-10-19 11:26:25 +0200

    Skills by level

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -3,6 +3,7 @@ const tags = parent.getElementsByTagName("li");
 const buttons = document.getElementsByClassName("skills__button");
 const buttonReLoad = document.querySelector(".skills__button--reload");
 const buttonSort = document.querySelector(".skills__button--sort");
+const buttonLevel = document.querySelector(".skills__button--level");
 const fragment = document.createDocumentFragment();
 
 const renderTags = () => {
@@ -15,7 +16,7 @@ const renderTags = () => {
 
 const filterTags = (category) => {
   for (let idx = 0; idx < tags.length; idx++) {
-    if (!category || tags[idx].dataset.skillsCategory === category) {
+    if (!category || tags[idx].dataset.skillCategory === category) {
       tags[idx].classList.remove("skills__item--faded");
     } else {
       tags[idx].classList.add("skills__item--faded");
@@ -25,17 +26,29 @@ const filterTags = (category) => {
 
 const sortTags = () => {
   Array.from(tags)
-    .sort((a, b) => a.textContent.localeCompare(b.textContent))
+    .sort((a, b) => {
+      return a.textContent.localeCompare(b.textContent);
+    })
+    .forEach((li) => parent.appendChild(li));
+};
+
+const sortTagsLevel = () => {
+  renderTags();
+  Array.from(tags)
+    .sort((a, b) => {
+      return b.dataset.skillLevel - a.dataset.skillLevel;
+    })
     .forEach((li) => parent.appendChild(li));
 };
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("mouseover", () => {
-    filterTags(buttons[i].dataset.skillsCategory);
+    filterTags(buttons[i].dataset.skillCategory);
   });
   buttons[i].addEventListener("mouseout", () => filterTags());
 }
 buttonReLoad.addEventListener("click", renderTags);
 buttonSort.addEventListener("click", sortTags);
+buttonLevel.addEventListener("click", sortTagsLevel);
 
 renderTags();
